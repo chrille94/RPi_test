9d55ad2dc1cca84eb4e3b18044bf9234e8d10a1d
chrille94 <31881849+chrille94@users.noreply.github.com>
2017-09-18 13:00:48 +0200

    Add files via upload

--- lfMultiple.py
+++ lfMultiple.py
@@ -34,12 +34,12 @@ def getSensors():
 def decideSpeed():
     sensorval = getSensors()
     print(sensorval)
-    # if(sensorval == [0, 0, 0] or [1, 1, 1] or [0, 1, 0]):
+    if(sensorval == [0, 0, 0]) or (sensorval == [1, 1, 1]) or (sensorval == [0, 1, 0]):
     #    direction = (100, 100)
-    if(sensorval == [1, 1, 1]):
-        direction = (10, 100)
+    # if(sensorval == [1, 1, 1]):
+        direction = (100, 100)
     else:
-        direction = (60, 100)
+        direction = (-100, 100)
     print(direction)
     # runMotor(direction)
 
