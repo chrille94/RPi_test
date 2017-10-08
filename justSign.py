3d0b173caa80ccc7ca3a75ab7e1b45e97d703d44
chrille94 <31881849+chrille94@users.noreply.github.com>
2017-10-08 11:10:58 +0200

    Add files via upload

---
+++ justSign.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+# importing modules
+import RPi.GPIO as GPIO
+import time
+
+# GPIO pin defs
+sign1 = 19
+sign5 = 26
+
+GPIO.setmode(GPIO.BCM)
+
+gpio_outputs = (sign1, sign5)
+
+GPIO.setup(gpio_outputs, GPIO.OUT)
+oneSign = GPIO.PWM(sign1, 400)
+fiveSign = GPIO.PWM(sign5, 400)
+
+def cleanupGpio():
+    GPIO.cleanup()
+
+try:
+    oneSign.start(0)
+    fiveSign.start(0)
+    while 1:
+        for i in range(0, 100, 1):
+            oneSign.ChangeDutyCycle(i)
+            time.sleep(0.01)
+
+        for j in range(100, 0, -1):
+            oneSign.ChangeDutyCycle(j)
+            time.sleep(0.01)
+
+except KeyboardInterrupt:
+    print("cleanup")
+    cleanupGpio()
